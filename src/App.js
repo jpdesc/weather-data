a68fbdea790127255d513507440ad80199f46b55
Jack DesCombes <jpdesc@alumni.stanford.edu>
2022-12-28 16:11:41 -0800

    Complete weather API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const apiKey = '1126ea142dfe633b53a669936a917613';
+    const [weatherData, setWeatherData] = useState([{}])
+    const [city, setCity] = useState("");
+
+    const getWeather = (event) => {
+        if (event.key === "Enter") {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`).then(
+                response => response.json()
+            ).then(
+                data => {
+                    setWeatherData(data)
+                    setCity("")
+                }
+            )
+        }
+    }
+
+    return (
+        <div className='container'>
+            <input
+                className='input'
+                placeholder="Enter City..."
+                onChange={e => setCity(e.target.value)}
+                value={city}
+                onKeyPress={getWeather}
+            />
+
+            {typeof weatherData.main === 'undefined' ? (
+                <div>
+                    <p>Welcome to the weather app! Enter in a city to get its weather.</p>
+                </div>
+            ) : (
+                <div className='weather-data'>
+                    <p className='city'>{weatherData.name}</p>
+                    <p className='temp'>{Math.round(weatherData.main.temp)} °F</p>
+                    <p className='weather'>{weatherData.weather[0].main}</p>
+                </div>
+            )}
+            {weatherData.cod === "404" ? (
+                <p>City not found.</p>) : (
+                    <>
+                    </>
+            )}
+            </div>
+  )
 }
 
-export default App;
+export default App
